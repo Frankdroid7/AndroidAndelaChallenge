8d8dbb29e09107d458bc420fb47171db597485d7
Vhictor <adeleyevictor40@gmail.com>
2019-07-14 12:30:11 -0500

    Intial commit

---
+++ app/src/main/java/ng/com/andelaandroidchallenge/franklin/ProfileActivity.java
@@ -0,0 +1,14 @@
+package ng.com.andelaandroidchallenge.franklin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
